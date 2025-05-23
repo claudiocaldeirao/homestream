b10b9ea563a55602f7a13ca50e2943696b99943f
Luiz Caldeirao <claudiocaldeirao@gmail.com>
2025-05-23 17:11:34 -0300

    feat: update image configuration to use remote patterns for dynamic hostname support

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["www.flaticon.com"], // substitua com o domínio real da sua URL
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
   },
 };
 
